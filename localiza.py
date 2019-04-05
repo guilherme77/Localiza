e4d0c716672fc88c57728ec8d4722cefada61212
Guilherme Da Silva Barros <31021612+guilherme77@users.noreply.github.com>
2019-04-05 11:57:27 -0300

    Create localiza.py

---
+++ localiza.py
@@ -0,0 +1,11 @@
+"""
+Discipline: PROGRAMMING LANGUAGE APPLIED TO AUTOMATION. - LPAA
+Teacher: Lucas Ponciano
+Students: Guilherme Barros
+
+Project of a system that performs registration, rent or sale to a car rental company in python.
+
+initiation of the program...
+"""
+
+def localiza():
