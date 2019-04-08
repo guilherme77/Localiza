3a97ca205c6e74b40409620b7516fba580f1884f
Lukas Pereira Ponciano Da Silva <31020951+lukasponci@users.noreply.github.com>
2019-04-08 00:44:11 -0300

    Update: funcao realizar_cadastro iniciada

--- localiza.py
+++ localiza.py
@@ -14,10 +14,10 @@ tag = True
 
 # Variaveis globais
 
-lst_clientes = [['ID', 'Nome', 'Login', 'Senha', 'CPF', 'idade', 'datanasc', 'divida_com_a_empresa']]
-lst_perfispendentes = [['ID', 'Nome', 'Login', 'Senha', 'CPF', 'idade', 'datanasc']]
-lst_funcionarios = [['ID', 'Nome', 'Login', 'Senha', 'CPF', 'idade', 'datanasc']]
-lst_gerentes = [['ID', 'Nome', 'Login', 'Senha', 'CPF', 'idade', 'datanasc']]
+lst_clientes = [['ID', 'Nome','CPF', 'idade', 'datanasc', 'Login', 'Senha', 'divida_com_a_empresa']]
+lst_perfispendentes = [['ID', 'Nome','CPF', 'idade', 'datanasc']]
+lst_funcionarios = [['ID', 'Nome','CPF', 'idade', 'datanasc', 'Login', 'Senha']]
+lst_gerentes = [['ID', 'Nome','CPF', 'idade', 'datanasc', 'Login', 'Senha']]
 lst_usuarios = [lst_gerentes ,lst_clientes, lst_funcionarios]
 lst_tarifasdiarias = [1,2,3]
 
@@ -25,17 +25,46 @@ lst_tarifasdiarias = [1,2,3]
 
 def main():
     print('Iniciando sistema ...\n')
-    print('Locadora de Vans Localiza, bem-vindo!\n')
+    print('Locadora de Vans Localiza, bem-vindo(a)!\n')
     
     inicia_sistema()
 
 def inicia_sistema():
     x = 0 # variavel provisoria, apenas para nao estourar o while
     while(tag):
-        print('colocar coisas primarias aqui')
+        opcoes_iniciais()
         x += 1
-        
-        if x == 7:
+        if x==1:
             break
+
+def opcoes_iniciais():
+    entrada_inicial = 0
+    tp_entrada = ('1','2')
+    
+    while(entrada_inicial not in tp_entrada):
+        entrada_inicial = raw_input('Opcoes do sistema. Digite: 1 para logar ou 2 para realizar cadastro\n')
+    
+    if entrada_inicial=='1':
+        print('o sport ta na final')
+    else:
+        realizar_cadastro()
+        
+    return 0
+
+def realizar_cadastro():
+    lst_perguntas = ['\nNome: ', '\nCPF: ', '\nIdade: ', '\nData de nascimento: ', '\nLogin desejado: ', '\nSenha escolhida: ']
+    lst_dados = []
+    x = 0
+    
+    print('\nEntrada de dados para cadastro. Responda conforme o que for pedido:')
+    while(x<6):
+        entrada_dados = raw_input('%s' %(lst_perguntas[x]))
+        lst_dados.append(entrada_dados)
+        x+=1
+    print(lst_dados)
+    
+    return
+    
+##    lst_perfispendente.append(lst_dados) aparentemente só funciona com a lista passando como parâmetro
         
 main()
