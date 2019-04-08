cfc93870f24951f0c924387cf197d7b2fe1ee1c7
Lukas Pereira Ponciano Da Silva <31020951+lukasponci@users.noreply.github.com>
2019-04-07 23:29:05 -0300

    add algumas listas + main e inicia_sistema()

--- localiza.py
+++ localiza.py
@@ -8,4 +8,35 @@ Project of a system that performs registration, rent or sale to a car rental com
 initiation of the program...
 """
 
-def localiza():
+# Tags
+
+tag = True
+
+# Variaveis globais
+
+lst_clientes = [['ID', 'Nome', 'Login', 'Senha', 'CPF', 'idade', 'datanasc', 'divida_com_a_empresa']]
+lst_perfispendentes = [['ID', 'Nome', 'Login', 'Senha', 'CPF', 'idade', 'datanasc']]
+lst_funcionarios = [['ID', 'Nome', 'Login', 'Senha', 'CPF', 'idade', 'datanasc']]
+lst_gerentes = [['ID', 'Nome', 'Login', 'Senha', 'CPF', 'idade', 'datanasc']]
+lst_usuarios = [lst_gerentes ,lst_clientes, lst_funcionarios]
+
+tp_tarifasdiarias = (1,2,3)
+
+# Funcoes e resto do codigo
+
+def main():
+    print('Iniciando sistema ...\n')
+    print('Locadora de Vans Localiza, bem-vindo!\n')
+    
+    inicia_sistema()
+
+def inicia_sistema():
+    x = 0 # variavel provisoria, apenas para nao estourar o while
+    while(tag):
+        print('colocar coisas primarias aqui')
+        x += 1
+        
+        if x == 7:
+            break
+        
+main()
